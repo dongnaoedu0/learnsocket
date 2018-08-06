09c63bc258eef24a68990f920c23dabff996c023
dongnaoedu0 <domokolo@163.com>
2018-08-06 11:08:11 +0800

    hello demo

---
+++ src/socket/demo.java
@@ -0,0 +1,14 @@
+package socket;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello demo");
+
+
+	}
+	public int add(int a,int b) {
+		return a+b;		
+	}
+}
