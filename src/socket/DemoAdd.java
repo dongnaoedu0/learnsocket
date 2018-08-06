1403de619a7d50de0941cdcf41951045ba3ca29c
dongnaoedu0 <domokolo@163.com>
2018-08-06 13:10:12 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ src/socket/DemoAdd.java
@@ -0,0 +1,14 @@
+package socket;
+
+public class DemoAdd {
+
+	public static void main(String[] args) {
+		System.out.println("¼Ó·¨ÔËËã");
+		add(3, 3);
+
+	}
+	private static void add(int a,int b) {
+		int sum=a+b;
+		System.out.println(sum);
+	}
+}
